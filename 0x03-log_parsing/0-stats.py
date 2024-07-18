d59ca93f1577104a7d1fc21bc00c1ab4be17ed1b
maiko254 <bonyomichael406.com>
2024-07-18 15:10:10 +0300

    Python script reading each line (containing status codes and file size) from stdin, validating the format then printing the total file size and number of each status code after every 10 lines or after Keyboard interruption signal is received

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import sys
+import re
+import signal
+
+
+pattern = (
+    r'^(\d+\.\d+\.\d+\.\d+) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] '
+    r'"(\w+) ([^"]+)" (\d+) (\d+)'
+)
+match_count = 0
+
+size = 0
+count_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+
+
+def print_stats():
+    global size, count_codes
+    print(f"File size: {size}")
+    for code in sorted(count_codes.keys()):
+        if count_codes[code] > 0:
+            print(f"{code}: {count_codes[code]}")
+
+
+def handle_interrupt(signal, frame):
+    print_stats()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, handle_interrupt)
+
+try:
+    for line in sys.stdin:
+        line = line.strip()
+        match = re.match(pattern, line)
+        if match:
+            match_count += 1
+            ip, tstamp, method, url, status_code, file_size = match.groups()
+            size += int(file_size)
+            count_codes[status_code] = count_codes.get(status_code, 0) + 1
+            if match_count % 10 == 0:
+                print_stats()
+        else:
+            continue
+except Exception as e:
+    print(f"Error: {e}")
+
+print_stats()
