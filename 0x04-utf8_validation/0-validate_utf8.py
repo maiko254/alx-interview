18e44c37904c8f31f87bc751096f0642b912f124
maiko254 <bonyomichael406.com>
2024-07-25 11:16:13 +0300

    Correction: Fixed code to align to PEP-8 style

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module checking if a dataset is represents a valid UTF-8 encoding"""
+
+
 def validUTF8(data):
     """Validate UTF-8 encoding of a sequence of characters
     """
