eb36509be06ee2b4e560beae7f836f6091fa9769
maiko254 <bonyomichael406.com>
2024-07-25 10:31:28 +0300

    Module validating UTF-8 encoding of a sequence of characters

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def validUTF8(data):
+    """Validate UTF-8 encoding of a sequence of characters
+    """
+    num_bytes = 0
+    for byte in data:
+        byte = byte & 0xFF
+        if num_bytes:
+            if byte >> 6 != 2:
+                return False
+            num_bytes -= 1
+            continue
+        while (1 << abs(7 - num_bytes)) & byte:
+            num_bytes += 1
+        if num_bytes == 1 or num_bytes > 4:
+            return False
+        num_bytes = max(num_bytes - 1, 0)
+    return num_bytes == 0
