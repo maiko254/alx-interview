67c371997aa720c5baaa37218fd2c96100b2091a
maiko254 <bonyomichael406.com>
2024-08-15 20:44:50 +0300

    Correction: Deleted imported Typing module

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Rotating a 2D matrix in-place 90 degrees clock-wise."""
-from typing import List, Tuple
 
 
-def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+def rotate_2d_matrix(matrix):
     """Rotates a 2D matrix in-place."""
     n = len(matrix)
     for i in range(n // 2):
